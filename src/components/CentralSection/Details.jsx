22dcc66e0352c0e4de81bc95fcba4d3059afd998
Franconobile <eifrann22@gmail.com>
2024-08-24 04:35:21 -0300

    More changes

--- src/components/CentralSection/Details.jsx
+++ src/components/CentralSection/Details.jsx
@@ -4,13 +4,33 @@ import styled from 'styled-components';
 
 const Modal = styled.div`
   position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: rgba(0, 0, 0, 0.8);
-  padding: 20px;
-  border-radius: 10px;
   z-index: 20;
+`;
+
+const ModalContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ChrContent = styled.div`
+  width: 300px;
+  height: 300px;
+  border: 1px solid #bbbbbb;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Mantinia', sans-serif;
 `;
 
 const ModalButton = styled.button`
@@ -21,14 +41,19 @@ const ModalButton = styled.button`
   border-radius: 5px;
   cursor: pointer;
   margin-top: 10px;
+  width: 50%;
 `;
 
 
 const Details = ({onClose}) => {
     return (
-        <Modal>
-        <p>Here's the character you pulled</p>
-        <ModalButton onClick={onClose}>Great!</ModalButton>
+      <Modal>
+        <ModalContent>
+            <ChrContent>
+                <p>Here's the character you pulled</p>
+            </ChrContent>
+            <ModalButton onClick={onClose}>Omg great!</ModalButton>
+        </ModalContent>
       </Modal>
     );
 };
